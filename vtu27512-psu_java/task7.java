0f7175ec8a917d903cc536ba58ce383946e7b270
vtu27512-mokshitha <vtu27512@veltech.edu.in>
2026-07-26 23:23:36 +0530

    Add files via upload

---
+++ vtu27512-psu_java/task7.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class task7 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        int esum=0;
+        int osum=0;
+        while(n!=0){
+            int rem=n%10;
+            n=n/10;  
+        if(rem%2==0){
+            esum=esum+rem;
+        }else{
+            osum=osum+rem;
+        }
+    } 
+            System.out.printf("even sum %d\n",esum);
+            System.out.printf("odd sum %d " ,osum);
+}
+}
