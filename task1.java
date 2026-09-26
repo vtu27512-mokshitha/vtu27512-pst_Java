ff09161de7870811a1b4cd3023d7a28dab9958d1
vtu27512-mokshitha <vtu27512@veltech.edu.in>
2026-09-26 12:03:50 +0530

    week7 pst

---
+++ task1.java
@@ -0,0 +1,12 @@
+public class task1 {
+    public static void main(String args[]){
+        String s1="hello";
+        for(int i=0;i<s1.length();i++){
+            s1.charAt(i);
+            System.out.println(i);
+        }
+        char s='B';
+        int ascii=s;
+        System.out.println(ascii);
+    }
+}
